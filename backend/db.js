553654efacdf7e3e53f17cb1048db15f241268c8
Zeeshan Siddiqui <mzeeshancs8990@gmail.com>
2026-06-16 21:33:04 +0500

    updated files 16-June-2026

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "----", 
-    database: "-----",
+    password: "SQLmzs2006", 
+    database: "finance_db",
     // Adding this ensures decimals (like amounts) are treated as numbers, not strings
     decimalNumbers: true 
 });
